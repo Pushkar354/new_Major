5efa8a5097da5204960b7ac801f766ba92cc82a3
Pushkar37 <pushkarsharma5555@gmail.com>
2026-04-16 16:07:19 +0530

    updating own directory

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 dotenv.config();
 
-// ✅ CORS FIRST
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
@@ -15,19 +15,19 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
+
 const user_router = require("./Routes/user_routes.js");
 app.use("/user", user_router);
 
-// DB connect
+
 Connect_todb();
 
-// Test route
+
 app.get("/", (req, res) => {
   res.send("Backend working"); // MUST send response
 });
 
-// Server
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
