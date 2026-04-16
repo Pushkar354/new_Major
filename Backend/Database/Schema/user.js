4da85a71fdf6bdce444320f42f0b7d5281cdc557
Pushkar354 <146190696+Pushkar354@users.noreply.github.com>
2026-04-16 15:59:38 +0530

    Update user.js

--- Backend/Database/Schema/user.js
+++ Backend/Database/Schema/user.js
@@ -1,16 +1,13 @@
 const moongose=require('mongoose');
 const { options } = require('pdfkit');
 
-const Connect_todb=()=>{
+Connect_todb=()=>{
 
-    moongose.connect(process.env.MONGO_URI).then(() => console.log("Database connected")).catch((error)=>{console.log(error)});
-    
+    moongose.connect(process.env.MONGO_URI,{ useNewUrlParser: true,
+    useUnifiedTopology: true}).then(() => console.log("Database connected")).catch((error)=>{console.log(error)});
 }
 
 const user_Schema=new moongose.Schema({
-    name:{type:String,
-        required:true
-    },
     email:{type:String,
         unique:true,
         required:true
@@ -31,28 +28,13 @@ const pdf_Schema = new moongose.Schema({
   modules: { type: Buffer, required: true }, // PDF binary data
   contentType: { type: String, default: 'application/pdf' }
 });
-<<<<<<< HEAD:Backend/Database/Schema/user.js
-// const Course_Schema=new moongose.Schema({
-//     email:String,
-//     topic:String,
-//     data:{type:Buffer,required:true},
-//     modules:[pdf_Schema],
-//      createdAt: { type: Date, default: Date.now }
-=======
 const Course_Schema=new moongose.Schema({
     email:String,
     topic:String,
     data:[pdf_Schema],
      createdAt: { type: Date, default: Date.now }
->>>>>>> 3fc1d624e1824bb0d3a8de905784454cb441fd50:Database/Schema/user.js
 
-// })
-const Course_Schema = new moongose.Schema({
-  email: String,
-  topic: String,
-  modules: { type: Array }, // ✅ allow videos array
-  createdAt: { type: Date, default: Date.now }
-});
+})
 const Module_quiz=new moongose.Schema({
   
         question:String,
